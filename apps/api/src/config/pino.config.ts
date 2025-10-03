a10550eb179d7c74912a256453adbb265f65df12
Pablo Henrique <pablohenriqdev@gmail.com>
2025-10-03 11:50:22 -0300

    refactor: ensure pino config type safety

--- apps/api/src/config/pino.config.ts
+++ apps/api/src/config/pino.config.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config'
+import { Params as PinoConfigParams } from 'nestjs-pino'
 
-export const pinoConfig = (config: ConfigService) => ({
+export const pinoConfig = (config: ConfigService): PinoConfigParams => ({
 	pinoHttp: {
 		level: config.get<string>('log.level', 'info'),
 		transport: config.get('nodeEnv') !== 'production' ? { target: 'pino-pretty' } : undefined,
