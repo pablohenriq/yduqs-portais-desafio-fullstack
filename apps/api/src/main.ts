fba7a3f692f5405308a4c6fdf1699dab59b9b15a
Pablo Henrique <pablohenriqdev@gmail.com>
2025-10-03 13:01:44 -0300

    feat: use the validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
@@ -8,6 +9,8 @@ import { AppModule } from '@/app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
 	const config = new DocumentBuilder()
 		.setTitle('YDUQS Portais Desafio Fullstack API')
